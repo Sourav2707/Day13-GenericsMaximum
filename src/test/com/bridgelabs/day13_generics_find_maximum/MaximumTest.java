16e8cd87d97b3a4504c5b5f7a87d9c2803849c03
Sourav2707 <smartsourav33@gmail.com>
2023-01-07 22:16:14 +0530

    UC1.1 Given Max Number at 1st Position return the Same Number

---
+++ src/test/com/bridgelabs/day13_generics_find_maximum/MaximumTest.java
@@ -0,0 +1,17 @@
+package test.com.bridgelabs.day13_generics_find_maximum; 
+
+import com.bridgelabs.day13_generics_find_maximum.Maximum;
+import org.junit.Assert;
+import org.junit.Test;
+public class MaximumTest {
+    //I'm goind to give 10, 20, 30
+@Test
+public void testFindMaximum() throws Exception {
+    Maximum maximum = new Maximum();
+    int value = maximum.findMaximum(30, 20, 10);
+    int expected = 30;
+    Assert.assertEquals(expected, value);
+} 
+
+
+} 
